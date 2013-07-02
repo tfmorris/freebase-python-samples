42420d0c572f74ce12fc4c1309e53b28793ab4c2
Tom Morris <tfmorris@gmail.com>
2013-07-02 09:53:54 -0400

    Fix topic method name

--- json-rpc/topic.py
+++ json-rpc/topic.py
@@ -6,7 +6,8 @@ import os
 api_key = open(os.environ['HOME'] + "/.freebase_api_key").read()
 url = 'https://www.googleapis.com/rpc'
 request = {
-            'method': 'freebase.topic', 
+            'method': 'freebase.topic.lookup', 
+            'jsonrpc' : '2.0',
             'apiVersion': 'v1', 
             'params': {
               'id': ['en','bob_dylan'],
